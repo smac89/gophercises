c24f78f2b99d96d0dd684ce3b90175522fce2261
smac89 <nobleman.code@gmx.com>
2023-01-13 21:03:04 -0600

    add implementation for database handler

--- urlshort/handler.go
+++ urlshort/handler.go
@@ -1,30 +1,54 @@
 package urlshort
 
 import (
+	"errors"
+	"fmt"
+	bolt "go.etcd.io/bbolt"
+	"gopkg.in/yaml.v3"
+	"io/fs"
+	"log"
 	"net/http"
+	"time"
 )
+
+type pathMap struct {
+	pathsToUrl []struct {
+		Path string `yaml:"path"`
+		Url  string `yaml:"url"`
+	}
+}
 
 // MapHandler will return an http.HandlerFunc (which also
 // implements http.Handler) that will attempt to map any
 // paths (keys in the map) to their corresponding URL (values
 // that each key in the map points to, in string format).
-// If the path is not provided in the map, then the fallback
+// If the Path is not provided in the map, then the fallback
 // http.Handler will be called instead.
 func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
-	//	TODO: Implement this...
-	return nil
+	if len(pathsToUrls) == 0 {
+		return fallback.ServeHTTP
+	}
+	log.Printf("Creating mappings: %v\n", pathsToUrls)
+	return func(w http.ResponseWriter, req *http.Request) {
+		if url, ok := pathsToUrls[req.URL.Path]; ok {
+			log.Printf("Redirecting %s to %s", req.URL.Path, url)
+			http.Redirect(w, req, url, http.StatusMovedPermanently)
+		} else {
+			fallback.ServeHTTP(w, req)
+		}
+	}
 }
 
 // YAMLHandler will parse the provided YAML and then return
 // an http.HandlerFunc (which also implements http.Handler)
 // that will attempt to map any paths to their corresponding
-// URL. If the path is not provided in the YAML, then the
+// URL. If the Path is not provided in the YAML, then the
 // fallback http.Handler will be called instead.
 //
 // YAML is expected to be in the format:
 //
-//     - path: /some-path
-//       url: https://www.some-url.com/demo
+//   - Path: /some-Path
+//     Url: https://www.some-url.com/demo
 //
 // The only errors that can be returned all related to having
 // invalid YAML data.
@@ -32,6 +56,51 @@ func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.Handl
 // See MapHandler to create a similar http.HandlerFunc via
 // a mapping of paths to urls.
 func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
-	// TODO: Implement this...
-	return nil, nil
+	pm := pathMap{}
+	if err := yaml.Unmarshal(yml, &pm); err != nil {
+		return nil, fmt.Errorf("failed to parse yaml: %v", err)
+	}
+	pathsToUrl := make(map[string]string)
+	for _, v := range pm.pathsToUrl {
+		pathsToUrl[v.Path] = v.Url
+	}
+	return MapHandler(pathsToUrl, fallback), nil
+}
+
+func BoltDbHandler(dbPath, dbBucket string, fallback http.Handler) (http.HandlerFunc, error) {
+	db, err := bolt.Open(dbPath, 0666, &bolt.Options{ReadOnly: true, Timeout: 1 * time.Second})
+	if errors.Is(err, fs.ErrNotExist) {
+		log.Printf("dbfile not found, using fallback\n%v\n", err)
+		return fallback.ServeHTTP, nil
+	}
+	if err != nil {
+		return nil, fmt.Errorf("failed to open db file\n%v", err)
+	}
+	defer func(db *bolt.DB) {
+		err := db.Close()
+		if err != nil {
+			log.Printf("failed to close the db\n%v\n", err)
+		}
+	}(db)
+	tx, err := db.Begin(false)
+	if err != nil {
+		return nil, fmt.Errorf("unable to start transaction\n%v", err)
+	}
+
+	defer func(tx *bolt.Tx) {
+		err := tx.Rollback()
+		if err != nil {
+			log.Printf("failed to rollback transaction\n%v\n", err)
+		}
+	}(tx)
+	c := tx.Bucket([]byte(dbBucket)).Cursor()
+	pathsToUrl := make(map[string]string)
+	for k, v := c.First(); k != nil; k, v = c.Next() {
+		pathsToUrl[string(k)] = string(v)
+	}
+	return MapHandler(pathsToUrl, fallback), nil
+}
+
+func (p *pathMap) UnmarshalYAML(unmarshal func(interface{}) error) error {
+	return unmarshal(&p.pathsToUrl)
 }
--- urlshort/main/main.go
+++ urlshort/main/main.go
@@ -1,13 +1,29 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"gophercises.com/urlshort"
+	"log"
 	"net/http"
+	"os"
+)
 
-	"github.com/gophercises/urlshort"
+const (
+	yamlPaths = `
+- path: /urlshort
+  url: https://github.com/gophercises/urlshort
+- path: /urlshort-final
+  url: https://github.com/gophercises/urlshort/tree/solution
+`
+	dbBucketName = "PathToUrl"
 )
 
 func main() {
+	var yamlFile, dbPath string
+	flag.StringVar(&yamlFile, "yaml-path", "", "Load path mappings from a yaml file")
+	flag.StringVar(&dbPath, "db-name", "bolt.db", "Load mappings from a bolt database")
+	flag.Parse()
 	mux := defaultMux()
 
 	// Build the MapHandler using the mux as the fallback
@@ -19,18 +35,24 @@ func main() {
 
 	// Build the YAMLHandler using the mapHandler as the
 	// fallback
-	yaml := `
-- path: /urlshort
-  url: https://github.com/gophercises/urlshort
-- path: /urlshort-final
-  url: https://github.com/gophercises/urlshort/tree/solution
-`
-	yamlHandler, err := urlshort.YAMLHandler([]byte(yaml), mapHandler)
-	if err != nil {
-		panic(err)
+	var yaml = []byte(yamlPaths)
+	if content, err := os.ReadFile(yamlFile); err == nil {
+		yaml = content[:]
+	} else if yamlFile != "" {
+		log.Printf("Unable to read file: %s.\nError: %v", yamlFile, err)
 	}
-	fmt.Println("Starting the server on :8080")
-	http.ListenAndServe(":8080", yamlHandler)
+	yamlHandler, err := urlshort.YAMLHandler(yaml, mapHandler)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Get some data from the db if possible
+	dbHandler, err := urlshort.BoltDbHandler(dbPath, dbBucketName, yamlHandler)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Starting the server on :8080")
+	log.Fatal(http.ListenAndServe(":8080", dbHandler))
 }
 
 func defaultMux() *http.ServeMux {
